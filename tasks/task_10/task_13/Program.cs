25a85331f08395b6167cbdea61ef90bdfad1f4af
TIM27 <sprinchak27@mail.ru>
2022-07-04 19:32:42 +0300

    .added task_13

---
+++ tasks/task_10/task_13/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа, или сообщает что третьей цифры нет.
+// 456-->5
+// 782-->8
+// 918-->1
+
+namespace Task10
+{
+    class Program1
+    {
+        static void Main (string[] args )
+
+        {
+
+            Console.WriteLine("Введите число : ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            string NumberТext = Convert.ToString(Number);
+            if (NumberТext.Length > 2)
+            {
+                Console.WriteLine("Результат " + NumberТext [2]);
+            }
+            else
+            {
+                Console.WriteLine("Третья цифра отсутствует ");
+            }
+
+      
+             
+    
+
+
+            }
+        }
+    
+}
