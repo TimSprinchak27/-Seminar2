8829dbad1fe5372e8adf8ed4b218f91078fd4d33
TIM27 <sprinchak27@mail.ru>
2022-07-04 19:15:15 +0300

    .added task_10

---
+++ tasks/task_10/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая на вход принимает трехзначное число и на выходе показывает вторую цифру этого числа.
+// 456-->5
+// 782-->8
+// 918-->1
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main (string[] args )
+
+        {
+
+            Console.WriteLine("Введите трехзначное число : ");
+            int ТрехзначноеЧисло = Convert.ToInt32(Console.ReadLine());
+            string stringNumber = Convert.ToString(ТрехзначноеЧисло);
+            
+            Console.WriteLine("Результат " + stringNumber [1]);
+
+            
+             
+            
+
+
+
+
+            }
+        }
+    
+}
+
