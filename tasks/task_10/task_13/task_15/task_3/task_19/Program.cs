2d4f57cd6696df7d741737c71d420e95618894ea
TIM27 <sprinchak27@mail.ru>
2022-07-07 22:47:38 +0300

    .added task_19

---
+++ tasks/task_10/task_13/task_15/task_3/task_19/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+namespace Task19
+{
+    class Program
+    {
+         static void Main (string[] args ) 
+
+        {
+
+            Console.WriteLine("Введите пятизначное число: ");
+            string? number = Console.ReadLine();
+
+            
+            if (number [0] == number [3] || number [1] == number [4])
+            {
+                Console.WriteLine($"Число: {number} - является палиндромом ");
+            }
+
+             else Console.WriteLine($"Число: {number} - не является палиндромом ");
+
+            if (number.Length == 5);
+            
+             else  Console.WriteLine("Введите корректное число");
+            
+              
+             
+
+
+            }
+        }
+    
+}
+
+
+
